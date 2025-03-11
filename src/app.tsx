2c62564b362c26532307ce996bb4ce5621173544
Sam Neale <me@samneale.dev>
2025-03-11 14:16:59 +1100

    navbar and footer

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "./assets/styles/fonts.css";
 import "./App.css";
 
@@ -13,9 +14,17 @@ function App() {
   }
 
   return (
-    <>
-      <Navbar page={page} />
-    </>
+    <div style={{ overflowY: "hidden" }} className="flex h-screen flex-col">
+      <div className="flex-none">
+        <Navbar page={page} setPage={setPage} />
+      </div>
+      <div className="grow" style={{ backgroundColor: "red" }}>
+        abc
+      </div>
+      <div className="flex-none">
+        <Footer />
+      </div>
+    </div>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,77 +1,106 @@
-function Navbar(props: { page: string }) {
+import { Camera, CodeSlash, EnvelopeAtFill } from "react-bootstrap-icons";
+
+function Navbar(props: { page: string; setPage: (page: string) => void }) {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+              <li>
+                <a onClick={() => props.setPage("projects")}>
+                  <CodeSlash /> Projects
+                </a>
+              </li>
+              <li>
+                <a onClick={() => props.setPage("captured")}>
+                  <Camera /> Captured
+                </a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
+
+          <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Item 1</a>
+              <a onClick={() => props.setPage("projects")}>
+                <CodeSlash /> Projects
+              </a>
             </li>
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
+              <a onClick={() => props.setPage("captured")}>
+                <Camera /> Captured
+              </a>
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <div className="navbar-center hidden lg:flex">
+          <a
+            onClick={() => props.setPage("")}
+            className="btn btn-ghost text-3xl baskervville fw-bold"
+          >
+            Sam Neale
+          </a>
+        </div>
+        <div className="navbar-end">
+          <button
+            className="btn btn-primary"
+            onClick={() =>
+              (document.getElementById(
+                "contactModal"
+              ) as HTMLDialogElement)!.showModal()
+            }
+          >
+            <EnvelopeAtFill />
+            Contact Me
+          </button>
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
-      </div>
-    </div>
+      <dialog id="contactModal" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">How to Contact Me</h3>
+          <p className="py-4">
+            Email me at{" "}
+            <a
+              className="link link-info font-bold"
+              href="mailto:me@samneale.dev"
+            >
+              me@samneale.dev
+            </a>
+            . I'm open to project commissions, inquiries, collaborations, or any
+            software-related questions. Looking forward to hearing from you!
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
   );
 }
 
